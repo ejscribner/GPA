e4dedf501c77deaf28cdd5940541c7c324ee49a2
Jordan Cutler <jdc219@lehigh.edu>
2017-11-28 19:15:30 -0500

    Maven changes made

--- src/main/java/com/escribner/gpa/mainForm.java
+++ src/main/java/com/escribner/gpa/mainForm.java
@@ -1,16 +1,6 @@
-/*
-    Created By: Elliot J Scribner on 11/24/17
-    Student ID: ejs320
-    Lab #: **Num**
-    mainForm: **Description**
- */
-
-package com.gpa;
-
-import sun.nio.cs.ext.MacThai;
+package com.escribner.gpa;
 
 import javax.swing.*;
-import java.applet.Applet;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -34,7 +24,6 @@ public class mainForm {
 
     public mainForm() {
         submit.addActionListener(new ActionListener() {
-            @Override
             public void actionPerformed(ActionEvent e) {
                 String myCred = cred1.getText();
                 String[] grades = {grade1.getText(), grade2.getText(), grade3.getText(), grade4.getText(), grade5.getText(), grade6.getText(), grade7.getText()};
@@ -98,7 +87,6 @@ public class mainForm {
         return numGrade;
     }
 
-
     public static void main(String[] args) {
         JFrame frame = new JFrame("frame");
         frame.setContentPane(new mainForm().mainPanel);
@@ -107,3 +95,4 @@ public class mainForm {
         frame.setVisible(true);
     }
 }
+
